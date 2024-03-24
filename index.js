c98a76ca088fc5e335c674516f35b6269a8fd8bb
Ian <ian.mwangi1@student.moringaschool.com>
2024-03-24 12:15:21 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function expression called divide
+const divide = function(a, b) {
+    return a / b;
+};
+
+// Arrow function called square
+const square = (x) => {
+    return a * b;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+};
+
+// Test cases
+console.log(divide(2000, 100)); // Output: 20
+console.log(square(5)); // Output: 25
+console.log(add(3, 7)); // Output: 10
